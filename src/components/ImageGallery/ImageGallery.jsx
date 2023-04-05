74c88ff775553ca8481c81734f4f21ffebe8842c
shuninamarina <marinashunina03@gmail.com>
2023-04-06 00:02:04 +0300

    commit

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,17 @@
+export const ImageGallery = ({ images, openModal }) => {
+  return (
+    <ul>
+      {images.map(image => {
+        return (
+          <li key={image.id}>
+            <img
+              src={image.webformatURL}
+              alt={image.tags}
+              onClick={() => openModal(image.largeImageURL)}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
